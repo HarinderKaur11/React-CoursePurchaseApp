a3f2bf9a7687ba936fe4f14ee44c445c33250c24
Harinder Kaur <snazyahuja11@gmail.com>
2017-08-29 16:51:50 -0400

    Add functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Coursesales from './Coursesales';
 
 class App extends Component {
   render() {
+
+    var courses=[
+      {name: 'Complete frontend development course', price: 150},
+      {name: 'Learn React with Redux', price: 200},
+      {name: 'Full stack development with MEAN stack', price: 205},
+      {name: 'Advanced Javascript with ES6', price: 90},
+      {name: 'React Native fundamentals', price: 115}
+    ];
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Course Sales page</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Coursesales items={courses}/>
       </div>
     );
   }
